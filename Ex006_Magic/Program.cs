5fdf9bdbb5e4bf0f51dbc9105b7e66fab01504a5
KarolinaTad <k.t.1993@bk.ru>
2023-12-06 13:23:52 +0300

    New 006

---
+++ Ex006_Magic/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+
+int xa = 1; ya = 1;
+    xb = 1; yb = 30;
+    xc = 40; yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Concole.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Concole.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Concole.WriteLine("+");
