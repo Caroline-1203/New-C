dcde0e07b1103d4648d877f638945e4164f6a8b3
KarolinaTad <k.t.1993@bk.ru>
2023-12-15 00:27:49 +0300

    3 tasks

---
+++ HW/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+Random random = new Random();
+int [] numbers = new int[10];
+int countInRange = 0;
+void FillArray (int[] numbers)
+{
+    int length = numbers.Length; 
+    int index = 0;
+    while(index < length)
+        {numbers[index] = new Random().Next(1,101);
+        index++;
+        {
+
+    numbers[index] = index + 1;
+
+}
+    }
+
+   
+{
+countInRange = 0;
+for (int i = 0; i < 10; i++)
+    if (numbers[index] >= 20 && numbers[index] <= 90)
+    {
+        countInRange++;
+        }
+FillArray(numbers);
+Console.WriteLine();
+Console.WriteLine("Количество элементов массива в интервале [20,90]: " + countInRange);
+    }
+}
+
+
+
---
+++ test1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 4**(не обязательно): Дано натуральное число в диапазоне от 1 до 100 000. Создайте массив, состоящий из цифр этого числа. 
+// Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. 
+// Размер массива должен быть равен количеству цифр.
+Random random=new Random();
+int X = random.Next (1,100000);// Задаем число
+System.Console.WriteLine();
+System.Console.WriteLine(X);
+
+int M=1;// Колличество цифр в числе X
+int N  = 9;
+int P = 1;
+// считаем колличество цифр
+for(int i=0; i<N;i++)
+{
+P = P* 10;
+if(X / P >= 1)
+M++;
+}
+
+int[] a=new int[M];// создаем массив 
+ P= 1;
+for(int i=0; i<M;i++)
+{
+for (int j= 1; j<M-i; j++)
+{
+P= P*10;
+}
+a[i]= (X/P)%10 ;
+P=1;
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine(M);
+for( int i=0;i<a.Length;i++)
+{
+System.Console.Write($"{a[i], 4}");
+}
