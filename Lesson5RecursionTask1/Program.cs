cea009ef95ea857e544c0a158051ddd11e260977
KarolinaTad <k.t.1993@bk.ru>
2023-12-30 00:16:31 +0300

    For debugging

---
+++ Lesson5RecursionTask1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Вычислить факториал от натурального числа N.
+
+
+// Это все пример для учебы. Есть вариант получше. Будет ниже закомментировано.
+
+int Fact(int n)
+{
+    if(n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+    return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+Console.Write(Fact(5)); 
+
+
+// Лучший вариант
+
+// int Fact(int n)
+// {
+//     int result = 1;
+//     for (int i = 2; i <= n; i++)
+//     {
+//         result *= i;
+//     }
+//     return result;
+// }
